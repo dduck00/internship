cbf52025c9e4ebf43f18f30daf2072a23b0b34d3
이상덕 <ejr075@gmail.com>
2019-07-17 15:10:14 +0900

    promotion

--- src/main/java/com/nts/reservation/service/impl/PromotionServiceTest.java
+++ src/main/java/com/nts/reservation/service/impl/PromotionServiceTest.java
@@ -4,24 +4,21 @@ import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.nts.reservation.config.ApplicationConfig;
 import com.nts.reservation.dao.PromotionDao;
 import com.nts.reservation.dto.FileInfo;
 import com.nts.reservation.dto.ProductImage;
 import com.nts.reservation.dto.Promotion;
-import com.nts.reservation.service.PromotionService;
 
-@Service
-public class PromotionServiceImp implements PromotionService {
-	@Autowired
-	PromotionDao promotionDao;
+public class PromotionServiceTest {
 
-	@Override
-	@Transactional
-	public String getPromotionList() {
+	public static void main(String[] args) {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		PromotionDao promotionDao = ac.getBean(PromotionDao.class);
+
 		List<FileInfo> fileInfo = promotionDao.selectProductFileInfo();
 		List<ProductImage> productImage = promotionDao.selectPromotionImage();
 		List<Promotion> promotion = promotionDao.selectPromotion();
@@ -30,13 +27,14 @@ public class PromotionServiceImp implements PromotionService {
 
 		for (int indexOfPromotion = 0; indexOfPromotion < promotion.size(); indexOfPromotion++) {
 			JSONObject tempJson = new JSONObject();
+
 			tempJson.put("id", promotion.get(indexOfPromotion).getId());
 			tempJson.put("productId", productImage.get(indexOfPromotion).getProductId());
-			tempJson.put("productImageUrl", fileInfo.get(indexOfPromotion).getSaveFileName());
+			tempJson.put("productImageUrl", fileInfo.get(indexOfPromotion).getFileName());
 
 			promotionData.add(tempJson);
 		}
-		return promotionData.toJSONString();
+		System.out.println(promotionData.toJSONString());
 	}
 
 }
