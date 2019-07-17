5dc8acbfb2a09051a3718a1ba20a1a295c99d8e9
이상덕 <ejr075@gmail.com>
2019-07-17 15:48:39 +0900

    controller 초기화

--- src/main/java/com/nts/reservation/controller/ApiSet.java
+++ src/main/java/com/nts/reservation/controller/ApiSet.java
@@ -3,6 +3,7 @@ package com.nts.reservation.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nts.reservation.service.PromotionService;
@@ -16,6 +17,21 @@ public class ApiSet {
 
 	@GetMapping("/promotions")
 	public String getPromotion() {
+		String promotionJSON = "{\"items\": " + promotionService.getPromotionList() + "}";
+
+		return promotionJSON;
+	}
+
+	@GetMapping("/categories")
+	public String getCategories() {
+		String promotionJSON = "{\"items\": " + promotionService.getPromotionList() + "}";
+
+		return promotionJSON;
+	}
+
+	@GetMapping("/products")
+	public String getProducts(@RequestParam(name = "categoryId", required = false, defaultValue = "0") int categoryId,
+		@RequestParam(name = "start", required = false, defaultValue = "0") int start) {
 		String promotionJSON = "{\"items\": " + promotionService.getPromotionList() + "}";
 
 		return promotionJSON;
