14971cfe100b7f74bf223fa177e5be6899a130af
이상덕 <ejr075@gmail.com>
2019-07-18 17:53:20 +0900

    ajax js 파일 간소화

--- src/main/webapp/js/ajaxSetting.js
+++ src/main/webapp/js/ajaxSetting.js
@@ -1,3 +1,29 @@
+const TEMPLATE_PROMOTION = `<li class="item" style="background-image: url(/{productImageUrl});">
+<a href="#">
+    <span class="img_btm_border"></span>
+    <span class="img_right_border"></span>
+    <span class="img_bg_gra"></span>
+    <div class="event_txt">
+        <h4 class="event_txt_tit"></h4>
+        <p class="event_txt_adr"></p>
+        <p class="event_txt_dsc"></p>
+    </div>
+</a>
+</li>`;
+
+const TEMPLATE_PRODUCT = `<li class="item">
+<a href="/detail?id={id}" class="item_book">
+    <div class="item_preview">
+        <img alt="{description}" class="img_thumb" src="/{img}">
+        <span class="img_border"></span>
+    </div>
+    <div class="event_txt">
+        <h4 class="event_txt_tit"> <span>{description}</span> <small class="sm">{placeName}</small> </h4>
+        <p class="event_txt_dsc">{content}</p>
+    </div>
+</a>
+</li>`
+
 const requestProduct = new XMLHttpRequest();
 const requestPromotion = new XMLHttpRequest();
 const requestCategory = new XMLHttpRequest();
@@ -45,3 +71,4 @@ function ajaxSetting() {
         }
     }
 }
+
