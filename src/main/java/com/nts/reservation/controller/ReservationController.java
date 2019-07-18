61fd7b076e2e2f5716dbbe20d85ec37cab888881
이상덕 <ejr075@gmail.com>
2019-07-18 11:26:13 +0900

    상대경로 수정

--- src/main/java/com/nts/reservation/controller/ReservationController.java
+++ src/main/java/com/nts/reservation/controller/ReservationController.java
@@ -1,8 +1,17 @@
 package com.nts.reservation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReservationController {
+	@GetMapping(path = "/detail")
+	public String list() {
+		return "detail";
+	}
 
+	@GetMapping(path = "/bookinglogin")
+	public String bookinglogin() {
+		return "bookinglogin";
+	}
 }
