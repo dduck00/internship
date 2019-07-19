f770d356116c52925faf7e1e08e93164d6fe1208
dduck00 <ejr075@gmail.com>
2019-07-19 18:24:10 +0900

    추가

--- src/main/java/com/nts/reservation/dao/PromotionDao.java
+++ src/main/java/com/nts/reservation/dao/PromotionDao.java
@@ -13,6 +13,7 @@ import com.nts.reservation.dto.Promotion;
 
 @Repository
 public class PromotionDao {
+
 	static final private String SELECT_PROMOTION = "SELECT promotion.id AS id, product_image.product_id AS product_id, file_info.save_file_name AS product_image_url "
 		+ "FROM promotion INNER JOIN product_image INNER JOIN file_info "
 		+ "ON promotion.product_id = product_image.product_id AND product_image.file_id = file_info.id AND product_image.type = 'th'";
