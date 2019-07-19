5d4ec9859375019ca110739c8457a7d63a37369d
dduck00 <ejr075@gmail.com>
2019-07-19 11:56:29 +0900

    jdbc.properties 추가

--- src/main/java/com/nts/reservation/config/ApplicationConfig.java
+++ src/main/java/com/nts/reservation/config/ApplicationConfig.java
@@ -1,12 +1,20 @@
 package com.nts.reservation.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = {"com.nts.reservation.dao", "com.nts.reservation.service"})
+@PropertySource(value = {"/WEB-INF/properties/jdbc.properties"})
 @Import({DBConfig.class})
 public class ApplicationConfig {
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 }
