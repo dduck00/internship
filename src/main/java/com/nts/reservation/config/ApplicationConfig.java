f35021fdd098ce7e00fc26fcb3c003085aed9af3
이상덕 <ejr075@gmail.com>
2019-07-21 15:13:32 +0900

    config변경

--- src/main/java/com/nts/reservation/config/ApplicationConfig.java
+++ src/main/java/com/nts/reservation/config/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan(basePackages = {"com.nts.reservation.dao", "com.nts.reservation.service"})
+@ComponentScan(basePackages = {"com.nts.reservation"})
 @PropertySource(value = {"classpath:properties/jdbc.properties"})
 @Import({DBConfig.class})
 public class ApplicationConfig {
