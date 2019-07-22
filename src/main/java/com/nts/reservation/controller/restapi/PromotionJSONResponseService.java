f28a677b83c0031d8b467aefd56fc5da56740b83
dduck00 <ejr075@gmail.com>
2019-07-22 10:18:25 +0900

    네이버 컨벤션 적용

--- src/main/java/com/nts/reservation/controller/restapi/PromotionJSONResponseService.java
+++ src/main/java/com/nts/reservation/controller/restapi/PromotionJSONResponseService.java
@@ -1,13 +1,10 @@
 package com.nts.reservation.controller.restapi;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nts.reservation.dto.database.Promotion;
 import com.nts.reservation.dto.response.PromotionResponse;
 import com.nts.reservation.service.PromotionService;
 
