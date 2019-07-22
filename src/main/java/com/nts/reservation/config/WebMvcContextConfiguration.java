4bef2bede1ab47ed8c5bf887b51d8b76ff0ff7cd
이상덕 <ejr075@gmail.com>
2019-07-22 13:56:57 +0900

    resource url 변경

--- src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
+++ src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
@@ -17,11 +17,7 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/").setCachePeriod(31556926);
-		registry.addResourceHandler("/img/**").addResourceLocations("/resources/img/").setCachePeriod(31556926);
-		registry.addResourceHandler("/img_map/**").addResourceLocations("/resources/img_map/").setCachePeriod(31556926);
-		registry.addResourceHandler("/fonts/**").addResourceLocations("/resources/fonts/").setCachePeriod(31556926);
-		registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/").setCachePeriod(31556926);
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
 
 	}
 
