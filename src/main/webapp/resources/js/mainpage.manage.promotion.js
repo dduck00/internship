3a7d8006fa544cfd8cdecf427d7725c519f28661
dduck00 <ejr075@gmail.com>
2019-07-22 12:04:49 +0900

    jQuery변경

--- src/main/webapp/resources/js/mainpage.manage.promotion.js
+++ src/main/webapp/resources/js/mainpage.manage.promotion.js
@@ -1,5 +1,5 @@
 function setPromotion(responseText) {
-    const promotionDataJSON = JSON.parse(responseText).items;
+    const promotionDataJSON = responseText.items;
     let inserteToHtml = "";
     for (let promotionData of promotionDataJSON) {
         inserteToHtml += TEMPLATE_PROMOTION.replace("{productImageUrl}", promotionData.productImageUrl)
@@ -9,29 +9,29 @@ function setPromotion(responseText) {
 
 function promotion_animation() {
     let count = 1;
-    let middleInit = PROMOTION_LIST.querySelector('li:nth-child(1)');
-    let backInit = PROMOTION_LIST.querySelector('li:nth-child(2)');
+    let middleInit = PROMOTION_LIST.children('li:nth-child(1)');
+    let backInit = PROMOTION_LIST.children('li:nth-child(2)');
     function run(front, middle, back) {
         if (front !== 0) {
-            front.style.transition = "";
-            front.style.left = "418px";
+            front.css('transition', '');
+            front.css('left', '418px');
         }
-        middle.style.left = "-418px";
-        back.style.left = "0px";
+        middle.css('left', '-418px');
+        back.css('left', '-0px');
         count++;
 
         front = middle;
         middle = back;
-        back = PROMOTION_LIST.querySelector(`li:nth-child(${count % 11 + 1})`)
+        back = PROMOTION_LIST.children(`li:nth-child(${count % 11 + 1})`)
         setTimeout(() => {
-            back.style.transition = "all 3s";
+            back.css('transition', 'all 3s');
             run(front, middle, back);
         }, 5000);
     }
 
-    middleInit.style.transition = "all 3s";
-    backInit.style.transition = "all 3s";
-    middleInit.style.left = "0px";
+    middleInit.css('transition', 'all 3s');
+    backInit.css('transition', 'all 3s');
+    middleInit.css('left', '0px');
     setTimeout(() => {
         run(0, middleInit, backInit);
     }, 5000)
