67d8be341d83275cb15586067dd5d9a02256db03
이상덕 <ejr075@gmail.com>
2019-07-23 12:25:04 +0900

    My

--- src/main/java/com/nts/reservation/service/impl/ProductServiceImp.java
+++ src/main/java/com/nts/reservation/service/impl/ProductServiceImp.java
@@ -3,17 +3,17 @@ package com.nts.reservation.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.nts.reservation.dao.ProductDao;
+import com.nts.reservation.dao.ProductDaoMyBatis;
 import com.nts.reservation.dto.response.ProductJSON;
 import com.nts.reservation.service.ProductService;
 
 @Service
 public class ProductServiceImp implements ProductService {
 
-	private ProductDao productDao;
+	private ProductDaoMyBatis productDao;
 
 	@Autowired
-	public ProductServiceImp(ProductDao productDao) {
+	public ProductServiceImp(ProductDaoMyBatis productDao) {
 		this.productDao = productDao;
 	}
 
@@ -29,13 +29,13 @@ public class ProductServiceImp implements ProductService {
 	public ProductJSON getProductJSON(int category, int start) {
 
 		ProductJSON productResponse = new ProductJSON();
-		productResponse.setTotalCount(getCount(category));
 
 		if (category <= 0 || category > 5) {
 			productResponse.setItems(productDao.getProductList(start, LIMIT));
 		} else {
 			productResponse.setItems(productDao.getProductListByCategory(category, start, LIMIT));
 		}
+		productResponse.setTotalCount(getCount(category));
 		return productResponse;
 
 	}
