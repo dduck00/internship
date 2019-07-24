265b426c40b091e0757f8f9e7142d8d68b31f01f
dduck00 <ejr075@gmail.com>
2019-07-24 10:22:12 +0900

    ProductList 추출 로직 변경

--- src/main/java/com/nts/reservation/service/impl/ProductServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/ProductServiceImpl.java
@@ -3,26 +3,35 @@ package com.nts.reservation.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nts.reservation.dao.CategoryDao;
 import com.nts.reservation.dao.ProductDao;
 import com.nts.reservation.dto.response.ProductJSON;
 import com.nts.reservation.service.ProductService;
 
 @Service
-public class ProductServiceImp implements ProductService {
+public class ProductServiceImpl implements ProductService {
 
 	private ProductDao productDao;
+	private CategoryDao categoryDao;
 
 	@Autowired
-	public ProductServiceImp(ProductDao productDao) {
+	public ProductServiceImpl(ProductDao productDao, CategoryDao categoryDao) {
 		this.productDao = productDao;
+		this.categoryDao = categoryDao;
 	}
 
 	@Override
 	public int getCount(int category) {
-		if (category <= 0) {
-			return productDao.selectProductCount();
+
+		if (isCorrectCategory(category) == false) {
+			return productDao.selectCountOfProductList();
 		}
-		return productDao.selectProductCountByCategory(category);
+
+		if (category == 0) {
+			return productDao.selectCountOfProductList();
+		}
+
+		return productDao.selectCountOfProductListByCategory(category);
 	}
 
 	@Override
@@ -30,7 +39,7 @@ public class ProductServiceImp implements ProductService {
 
 		ProductJSON productResponse = new ProductJSON();
 
-		if (category <= 0 || category > 5) {
+		if (isCorrectCategory(category)) {
 			productResponse.setItems(productDao.selectProductList(start, LIMIT));
 		} else {
 			productResponse.setItems(productDao.selectProductListByCategory(category, start, LIMIT));
@@ -41,4 +50,12 @@ public class ProductServiceImp implements ProductService {
 
 	}
 
+	private boolean isCorrectCategory(int category) {
+		int categoryCount = categoryDao.selectCategoryList().size();
+
+		if (0 <= category && category <= categoryCount) {
+			return true;
+		}
+		return false;
+	}
 }
