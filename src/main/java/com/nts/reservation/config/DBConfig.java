702a2cc5b37f5130ba74884b9a6c1a4bf4ff5090
이상덕 <ejr075@gmail.com>
2019-07-24 18:43:52 +0900

    불필요 어노테이션 제거

--- src/main/java/com/nts/reservation/config/DBConfig.java
+++ src/main/java/com/nts/reservation/config/DBConfig.java
@@ -6,10 +6,8 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 public class DBConfig {
 
 	@Value("${jdbc.DRIVER_CLASSNAME}")
