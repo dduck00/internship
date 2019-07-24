79ad1422e6feae4b4a1a4af319aefb56efdfd8c7
이상덕 <ejr075@gmail.com>
2019-07-24 23:59:55 +0900

    DBConfig 통합 ** 컨벤션 미적용

--- src/main/java/com/nts/reservation/config/ContextSqlMapper.java
+++ src/main/java/com/nts/reservation/config/ContextSqlMapper.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -16,10 +18,33 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 @MapperScan(basePackages = {"com.nts.reservation.dao"})
 public class ContextSqlMapper {
 
+	@Value("${jdbc.DRIVER_CLASSNAME}")
+	private String DRIVER_CLASSNAME;
+
+	@Value("${jdbc.URL}")
+	private String URL;
+
+	@Value("${jdbc.USERNAME}")
+	private String USERNAME;
+
+	@Value("${jdbc.PASSWORD}")
+	private String PASSWORD;
+
+	private DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(DRIVER_CLASSNAME);
+		dataSource.setUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		return dataSource;
+	}
+
+	
+	
 	@Bean
-	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws IOException {
+	public SqlSessionFactoryBean sqlSessionFactory() throws IOException {
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-		factoryBean.setDataSource(dataSource);
+		factoryBean.setDataSource(dataSource());
 		factoryBean
 			.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:/mybatis/*.xml"));
 		return factoryBean;
