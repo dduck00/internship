fac5aa8149e668d72439255147d402c1a04e21c6
dduck00 <ejr075@gmail.com>
2019-07-25 12:23:21 +0900

    Map 수정

--- src/main/java/com/nts/reservation/controller/ProductMapResponse.java
+++ src/main/java/com/nts/reservation/controller/ProductMapResponse.java
@@ -11,12 +11,12 @@ import com.nts.reservation.service.ProductService;
 
 @RestController
 @RequestMapping(path = "/api", produces = "application/json; charset=UTF-8")
-public class ProductSetResponse {
+public class ProductMapResponse {
 
 	private ProductService productService;
 
 	@Autowired
-	public ProductSetResponse(ProductService productService) {
+	public ProductMapResponse(ProductService productService) {
 		this.productService = productService;
 
 	}
