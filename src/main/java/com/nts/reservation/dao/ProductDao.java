a50fb25ceb96f19551979b3c2be65aa5b38afd9f
dduck00 <ejr075@gmail.com>
2019-07-29 16:59:06 +0900

    @Repository 어노테이션 추가

--- src/main/java/com/nts/reservation/dao/ProductDao.java
+++ src/main/java/com/nts/reservation/dao/ProductDao.java
@@ -3,9 +3,11 @@ package com.nts.reservation.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import com.nts.reservation.dto.Product;
 
+@Repository
 public interface ProductDao {
 	List<Product> selectProductList(@Param("id") int categoryId, @Param("start") int start, @Param("limit") int limit);
 
