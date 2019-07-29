27d3300aef6e75251d90de1a255400c3dbcdcd63
이상덕 <ejr075@gmail.com>
2019-07-29 16:59:06 +0900

    @Repository 어노테이션 추가

--- src/main/java/com/nts/reservation/dao/CategoryDao.java
+++ src/main/java/com/nts/reservation/dao/CategoryDao.java
@@ -2,8 +2,11 @@ package com.nts.reservation.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.nts.reservation.dto.Category;
 
+@Repository
 public interface CategoryDao {
 	List<Category> selectCategoryList();
 
--- src/main/java/com/nts/reservation/dao/PromotionDao.java
+++ src/main/java/com/nts/reservation/dao/PromotionDao.java
@@ -2,8 +2,11 @@ package com.nts.reservation.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.nts.reservation.dto.Promotion;
 
+@Repository
 public interface PromotionDao {
 	List<Promotion> selectPromotionList();
 }
