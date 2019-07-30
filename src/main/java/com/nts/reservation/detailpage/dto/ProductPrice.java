aefaef5a005202471b31f65239137511f6306d6d
dduck00 <ejr075@gmail.com>
2019-07-30 14:47:52 +0900

    DTO 추가

---
+++ src/main/java/com/nts/reservation/detailpage/dto/ProductPrice.java
@@ -0,0 +1,73 @@
+package com.nts.reservation.detailpage.dto;
+
+import java.sql.Timestamp;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("ProductPrice")
+public class ProductPrice {
+	private Timestamp createDate;
+	private int discountRate;
+	private Timestamp modifyDate;
+	private int price;
+	private String priceTypeName;
+	private int productId;
+	private int productPriceId;
+
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+
+	public int getDiscountRate() {
+		return discountRate;
+	}
+
+	public void setDiscountRate(int discountRate) {
+		this.discountRate = discountRate;
+	}
+
+	public Timestamp getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Timestamp modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getPriceTypeName() {
+		return priceTypeName;
+	}
+
+	public void setPriceTypeName(String priceTypeName) {
+		this.priceTypeName = priceTypeName;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getProductPriceId() {
+		return productPriceId;
+	}
+
+	public void setProductPriceId(int productPriceId) {
+		this.productPriceId = productPriceId;
+	}
+
+}
