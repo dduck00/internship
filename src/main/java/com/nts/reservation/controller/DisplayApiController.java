f8819e69eb78d89b1006f74297dead27712603c5
dduck00 <ejr075@gmail.com>
2019-07-31 14:19:32 +0900

    클래스명 변경

--- src/main/java/com/nts/reservation/controller/DisplayApiController.java
+++ src/main/java/com/nts/reservation/controller/DisplayApiController.java
@@ -6,22 +6,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nts.reservation.detailpage.dto.DisplayDetail;
+import com.nts.reservation.detailpage.dto.Display;
 import com.nts.reservation.detailpage.service.DisplayService;
 
 @RestController
 @RequestMapping(path = "/api", produces = "application/json; charset=UTF-8")
-public class DisplayDetailApiController {
+public class DisplayApiController {
 
 	private final DisplayService displayService;
 
 	@Autowired
-	public DisplayDetailApiController(DisplayService displayService) {
+	public DisplayApiController(DisplayService displayService) {
 		this.displayService = displayService;
 	}
 
 	@GetMapping("/products/{displayId}")
-	public DisplayDetail responseDisplayDetail(@PathVariable String displayId) {
+	public Display responseDisplayDetail(@PathVariable String displayId) {
 		return displayService.getDisplayDetail(Integer.parseInt(displayId));
 	}
 }
