149c45e89856a20f4bc8e5031a56784f69b7e56d
dduck00 <ejr075@gmail.com>
2019-08-01 14:17:59 +0900

    리뷰 페이지를 위한 dto, service component

--- src/main/java/com/nts/reservation/service/CommentService.java
+++ src/main/java/com/nts/reservation/service/CommentService.java
@@ -2,9 +2,13 @@ package com.nts.reservation.service;
 
 import java.util.List;
 
+import com.nts.reservation.dto.Comment;
 import com.nts.reservation.dto.CommentInfo;
 
 public interface CommentService {
+
+	Comment getComment(int productId);
+
 	List<CommentInfo> getCommentList(int productId);
 
 	double getCommentAverage(int productId);
