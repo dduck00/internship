9d506f928baf08533e7e190e7ee56339a4b0f386
dduck00 <ejr075@gmail.com>
2019-08-05 12:29:10 +0900

    문자열 에러처리

--- src/main/java/com/nts/reservation/controller/ExceptionHandlingController.java
+++ src/main/java/com/nts/reservation/controller/ExceptionHandlingController.java
@@ -1,5 +1,7 @@
 package com.nts.reservation.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,9 +10,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ExceptionHandlingController {
 
+	private final Logger loggerDisplayController = LoggerFactory.getLogger(DisplayApiController.class);
+
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler({IllegalArgumentException.class})
-	public void illegalArgumentHandler(IllegalArgumentException exception) {
-		exception.printStackTrace();
+	public void illegalArgumentHandler(IllegalArgumentException exception) {}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({NumberFormatException.class})
+	public void numberFormatHandler(NumberFormatException exception) {
+		loggerDisplayController.error("문자열이 들어왔습니다!");
 	}
 }
