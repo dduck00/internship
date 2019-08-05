57d6c9a68569fde57a39b4dc12591615856da88e
이상덕 <ejr075@gmail.com>
2019-08-05 12:22:09 +0900

    404 -> 400

--- src/main/java/com/nts/reservation/controller/ExceptionHandlingController.java
+++ src/main/java/com/nts/reservation/controller/ExceptionHandlingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ExceptionHandlingController {
 
-	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler({IllegalArgumentException.class})
 	public void illegalArgumentHandler(IllegalArgumentException exception) {
 		exception.printStackTrace();
