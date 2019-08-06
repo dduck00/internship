23d2be4c07b2920a85d071a8e5176e9be2b6765a
이상덕 <ejr075@gmail.com>
2019-08-06 11:05:08 +0900

    Spring Thymeleaf 적용

--- src/main/java/com/nts/reservation/controller/ReservationController.java
+++ src/main/java/com/nts/reservation/controller/ReservationController.java
@@ -1,18 +1,22 @@
 package com.nts.reservation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReservationController {
 
 	@GetMapping(path = "/product-detail")
-	public String productDetail() {
+	public String productDetail(@RequestParam(name = "id", required = true, defaultValue = "0") int id, Model model) {
+		model.addAttribute("displayId", id);
 		return "detail";
 	}
 
 	@GetMapping(path = "/product-review")
-	public String productReview() {
+	public String productReview(@RequestParam(name = "id", required = true, defaultValue = "0") int id, Model model) {
+		model.addAttribute("productId", id);
 		return "review";
 	}
 
