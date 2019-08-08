abe814befc71909ce2425ec3934347bb653fcaa3
dduck00 <ejr075@gmail.com>
2019-08-08 10:19:27 +0900

    설정 변경

--- src/main/webapp/resources/js/ajaxSetting.js
+++ src/main/webapp/resources/js/ajaxSetting.js
@@ -7,7 +7,5 @@ function AjaxInit() {
 function AjaxError(response, event) {
     if (response.status == 404) {
         alert('잘못된 공연정보를 요청하였습니다.');
-    } else if (response.status == 400) {
-        alert('잘못된 공연정보를 요청하였습니다.');
     }
 }
