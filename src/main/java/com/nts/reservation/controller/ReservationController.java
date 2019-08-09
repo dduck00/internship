a8c2fd3a09a01a857c96539a8b61edfdeb5bf9d4
이상덕 <ejr075@gmail.com>
2019-08-09 10:51:08 +0900

    생성자 주입

--- src/main/java/com/nts/reservation/controller/ReservationController.java
+++ src/main/java/com/nts/reservation/controller/ReservationController.java
@@ -3,20 +3,40 @@ package com.nts.reservation.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.nts.reservation.service.DisplayService;
+
 @Controller
 public class ReservationController {
 
+	private final DisplayService displayService;
+
+	@Autowired
+	public ReservationController(DisplayService displayService) {
+		this.displayService = displayService;
+	}
+
+	@GetMapping("/reserve")
+	public String reserve(Model model,
+		@CookieValue(value = "email", required = false) String cookieEmail,
+		@RequestParam(defaultValue = "0") int id) {
+
+		model.addAttribute("displayReserve", displayService.getDisplayReserve(id));
+		model.addAttribute("userEmail", cookieEmail);
+		return "reserve";
+	}
+
 	@GetMapping("/product-detail")
 	public String productDetail(Model model,
-			@CookieValue(value = "email", required = false) String cookieEmail, 
-			@RequestParam(defaultValue = "0") int id) {
-		
+		@CookieValue(value = "email", required = false) String cookieEmail,
+		@RequestParam(defaultValue = "0") int id) {
+
 		model.addAttribute("displayId", id);
 		model.addAttribute("userEmail", getEmailCookie(cookieEmail));
 		return "detail";
@@ -24,25 +44,32 @@ public class ReservationController {
 
 	@GetMapping("/product-review")
 	public String productReview(Model model,
-			@CookieValue(value = "email", required = false) String cookieEmail, 
-			@RequestParam(defaultValue = "0") int id) {
-		
+		@CookieValue(value = "email", required = false) String cookieEmail,
+		@RequestParam(defaultValue = "0") int id) {
+
 		model.addAttribute("productId", id);
 		model.addAttribute("userEmail", getEmailCookie(cookieEmail));
 		return "review";
 	}
 
-	@GetMapping("/booking-login")
-	public String bookingLogin() {
-		return "bookinglogin";
-	}
-
 	@GetMapping("/main-page")
 	public String mainPage(Model model,
 		@CookieValue(value = "email", required = false) String cookieEmail) {
-		
+
 		model.addAttribute("userEmail", getEmailCookie(cookieEmail));
 		return "mainpage";
+	}
+
+	private String getEmailCookie(String fromCookie) {
+		if (fromCookie == null) {
+			return "예약확인";
+		}
+		return fromCookie;
+	}
+
+	@GetMapping("/booking-login")
+	public String bookingLogin() {
+		return "bookinglogin";
 	}
 
 	@GetMapping("/myreservation")
@@ -59,13 +86,6 @@ public class ReservationController {
 		}
 
 		return "myreservation";
-	}
-	
-	private String getEmailCookie(String fromCookie) {
-		if(fromCookie == null) {
-			return "예약확인";
-		}
-		return fromCookie;
 	}
 
 }
