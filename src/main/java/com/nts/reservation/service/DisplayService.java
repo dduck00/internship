5602cbde940dd324687e8c8c5f39b2d316b167cc
이상덕 <ejr075@gmail.com>
2019-08-09 10:52:10 +0900

    Dao 추가

--- src/main/java/com/nts/reservation/service/DisplayService.java
+++ src/main/java/com/nts/reservation/service/DisplayService.java
@@ -1,7 +1,10 @@
 package com.nts.reservation.service;
 
 import com.nts.reservation.dto.Display;
+import com.nts.reservation.dto.DisplayReserve;
 
 public interface DisplayService {
 	Display getDisplay(int displayId);
+
+	DisplayReserve getDisplayReserve(int displayId);
 }
