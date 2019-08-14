f7e820f1c3e0e9810f5e428cca416918bd2712a3
dduck00 <ejr075@gmail.com>
2019-08-14 18:31:48 +0900

    불필요 로직 제거

--- src/main/java/com/nts/reservation/controller/ReservationApiController.java
+++ src/main/java/com/nts/reservation/controller/ReservationApiController.java
@@ -1,16 +1,12 @@
 package com.nts.reservation.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nts.reservation.dto.ReservationInfo;
 import com.nts.reservation.service.ReservationService;
 
 @RestController
@@ -24,15 +20,9 @@ public class ReservationApiController {
 		this.reservationService = reservationService;
 	}
 
-	@GetMapping("/reservations")
-	public Map<String, List<ReservationInfo>> responseReservationInfoList(
+	@PutMapping("/reservation")
+	public void cancelReservation(@RequestBody int id,
 		@CookieValue(value = "email") String email) {
-		return reservationService.getReservationMap(email);
-	}
-
-	@GetMapping("/reservation/{id}")
-	public void cancelReservation(@PathVariable int id,
-		@CookieValue(value = "email") String email) {
-		reservationService.cancelReservation(id, email);
+		reservationService.cancelReservation((id), email);
 	}
 }
