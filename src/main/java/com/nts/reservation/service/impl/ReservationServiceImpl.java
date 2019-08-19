36814379bce04d80f58bf31135aceba07d357438
dduck00 <ejr075@gmail.com>
2019-08-19 18:55:21 +0900

    Server Valid Check

--- src/main/java/com/nts/reservation/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/ReservationServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataRetrievalFailureException;
@@ -17,7 +18,9 @@ import com.nts.reservation.service.ReservationService;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
-
+	private static final Pattern EMAIL_PATTERN = Pattern.compile(
+		"/^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/");
+	private static final Pattern PHONE_PATTERN = Pattern.compile("/01([0-9])+-([0-9]{3,4})+-([0-9]){4}/");
 	private final ReservationDao reservationDao;
 
 	@Autowired
@@ -28,6 +31,18 @@ public class ReservationServiceImpl implements ReservationService {
 	@Override
 	@Transactional
 	public void addReservation(ReservationInfo reservationInfo) {
+
+		String email = reservationInfo.getReservationEmail();
+		String phone = reservationInfo.getReservationTelephone();
+
+		if (email.length() <= 0 || EMAIL_PATTERN.matcher(email).matches()) {
+			throw new IllegalArgumentException("Wrong Email");
+		}
+
+		if (phone.length() <= 0 || PHONE_PATTERN.matcher(phone).matches()) {
+			throw new IllegalArgumentException("Wrong Phone");
+		}
+
 		reservationDao.insertReservationInfo(reservationInfo);
 
 		reservationDao.insertReservationInfoPrice(reservationInfo);
