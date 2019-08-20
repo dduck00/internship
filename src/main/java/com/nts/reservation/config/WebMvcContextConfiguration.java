a22a98d8931fa17b921baa2c7762a058c1ae8fc4
dduck00 <ejr075@gmail.com>
2019-08-20 10:51:31 +0900

    File io 설정 추가

--- src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
+++ src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
@@ -3,6 +3,8 @@ package com.nts.reservation.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -55,5 +57,12 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 		viewResolver.setCharacterEncoding("UTF-8");
 		viewResolver.setTemplateEngine(templateEngine);
 		return viewResolver;
+	}
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(10485760);
+		return multipartResolver;
 	}
 }
