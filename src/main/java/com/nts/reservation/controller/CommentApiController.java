2b2b71bf79122d0b42305703253af2ca24fe5062
이상덕 <ejr075@gmail.com>
2019-08-20 15:25:19 +0900

    덧글 입력 컨트롤러 변경

--- src/main/java/com/nts/reservation/controller/CommentApiController.java
+++ src/main/java/com/nts/reservation/controller/CommentApiController.java
@@ -1,12 +1,21 @@
 package com.nts.reservation.controller;
 
+import java.time.LocalDate;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.nts.reservation.dto.CommentList;
+import com.nts.reservation.dto.FileInfo;
 import com.nts.reservation.service.CommentService;
 
 @RestController
@@ -23,6 +32,34 @@ public class CommentApiController {
 	@GetMapping("/comments/{productId}")
 	public CommentList responseCommentList(@PathVariable int productId) {
 		return commentService.getCommentList(productId);
+	}
+
+	@PostMapping("/add-comment/{productId}")
+	public ModelAndView addComment(@PathVariable int productId,
+		@CookieValue(value = "email") String cookieEmail,
+		@RequestParam(value = "file", required = false) MultipartFile file,
+		@RequestParam("comment") String comment) {
+
+		comment = StringUtils.stripToEmpty(comment);
+
+		commentService.addComment(buildFileInfo(file), cookieEmail, productId, comment);
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("redirect:/myreservation?resrv_email=" + cookieEmail);
+		return modelAndView;
+	}
+
+	private FileInfo buildFileInfo(MultipartFile file) {
+		LocalDate nowTime = LocalDate.now();
+
+		FileInfo fileInfo = new FileInfo();
+
+		fileInfo.setFileName(file.getOriginalFilename());
+		fileInfo.setContentType(file.getContentType());
+		fileInfo.setCreateDate(nowTime);
+		fileInfo.setModifyDate(nowTime);
+
+		return fileInfo;
 	}
 
 }
