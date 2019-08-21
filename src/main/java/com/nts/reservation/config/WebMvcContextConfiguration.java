55eca35b6484dad24fa495ff5107be55573d224e
이상덕 <ejr075@gmail.com>
2019-08-21 12:29:35 +0900

    파일 입출력 예외처리 및 경로 지정

--- src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
+++ src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
@@ -21,7 +21,12 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
+		registry.addResourceHandler("/resources/css/").addResourceLocations("/resources/css/").setCachePeriod(31556926);
+		registry.addResourceHandler("/resources/font/").addResourceLocations("/resources/font/")
+			.setCachePeriod(31556926);
+		registry.addResourceHandler("/resources/js/").addResourceLocations("/resources/js/").setCachePeriod(31556926);
+		registry.addResourceHandler("/resources/img_map/").addResourceLocations("/resources/img_map/")
+			.setCachePeriod(31556926);
 	}
 
 	@Override
