51c62d96c27d5de0e8d246431054103557bb6536
dduck00 <ejr075@gmail.com>
2019-08-23 10:15:01 +0900

    로직 간소화를 위해 트랜잭션 어노테이션 위치 변경

--- src/main/java/com/nts/reservation/service/impl/CommentServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/CommentServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.nts.reservation.dao.CommentDao;
 import com.nts.reservation.dto.CommentInfo;
@@ -71,7 +70,6 @@ public class CommentServiceImpl implements CommentService {
 	}
 
 	@Override
-	@Transactional
 	public void addCommentDB(FileInfo fileInfo, CommentInfo commentInfo) {
 		commentDao.insertComment(commentInfo);
 
