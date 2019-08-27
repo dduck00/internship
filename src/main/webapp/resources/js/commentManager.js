cc381e575c2741e13b9e81b47cb7f257155921d7
dduck00 <ejr075@gmail.com>
2019-08-27 17:14:53 +0900

    코멘트 개행처리

--- src/main/webapp/resources/js/commentManager.js
+++ src/main/webapp/resources/js/commentManager.js
@@ -26,7 +26,7 @@ const commentList = (comment, reservationDate) => {
     <div class="review_area ${imageToggle}">
         ${imageCode}
         <h4 class="resoc_name">${displayDescription}</h4>
-        <p class="review" >${comment.comment.replace(/[&<>"'`=\/]/g, (s) => { return escapeCharMap[s]; })}</p>
+        <p class="review" >${comment.comment.replace(/[&<>"'`=\/]/g, (s) => { return escapeCharMap[s]; }).replace(/[\n]/g, '<br>')}</p>
     </div>
     <div class="info_area">
         <div class="review_info"> <span class="grade">${comment.score}</span> <span
